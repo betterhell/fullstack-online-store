21c58587971444a371ba1dd4d18fff5e6406fedc
betterhell <skkogame@gmail.com>
2023-01-30 10:57:02 +0300

    Initial commit

---
+++ server/models/models.js
@@ -0,0 +1,78 @@
+const sequelize = require("../db")
+const {DataTypes} = require("sequelize")
+
+const User = sequelize.define("user", {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: "USER"},
+})
+
+const Basket = sequelize.define("basket", {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const BasketProduct = sequelize.define("basketProduct", {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const Product = sequelize.define("product", {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    price: {type: DataTypes.INTEGER, allowNull: false},
+    rating: {type: DataTypes.INTEGER, defaultValue: 0},
+    images: {type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false},
+})
+
+const Brand = sequelize.define("brand", {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+})
+
+const ProductInfo = sequelize.define("productInfo", {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    title: {type: DataTypes.STRING, allowNull: false},
+    description: {type: DataTypes.STRING, allowNull: false},
+})
+
+const Rating = sequelize.define("rating", {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    rate: {type: DataTypes.INTEGER, allowNull: false},
+})
+
+// Relations
+
+// User
+User.hasOne(Basket)
+Basket.belongsTo(User)
+
+User.hasMany(Rating)
+Rating.belongsTo(User)
+
+//Basket
+Basket.hasMany(BasketProduct)
+BasketProduct.belongsTo(Basket)
+
+// Brand
+Brand.hasMany(Product)
+Product.belongsTo(Brand)
+
+// Product
+Product.hasMany(Rating)
+Rating.belongsTo(Product)
+
+Product.hasMany(BasketProduct)
+BasketProduct.belongsTo(Product)
+
+Product.hasOne(ProductInfo)
+ProductInfo.belongsTo(Product)
+
+module.exports = {
+    User,
+    Basket,
+    BasketProduct,
+    Product,
+    Brand,
+    Rating,
+    ProductInfo
+}
