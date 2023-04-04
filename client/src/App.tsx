437e41ba1d57b7058a843d9b4d7156e1b77e8231
betterhell <dev.betterhell@gmail.com>
2023-04-04 13:48:42 +0300

    creating NotFound page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,26 @@
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Main from "./pages/Main/Main";
+import Header from "./components/Header/Header";
+import Actions from "./pages/Actions/Actions";
+import NotFound from "./pages/NotFound/NotFound";
+import React from "react";
+import MainPage from "./pages/Main/MainPage";
 
 const App = () => {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <div className="mainPage-content">
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/actions" element={<Actions />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
---
+++ client/src/pages/Main/MainPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./MainPage.module.scss";
+const MainPage = () => {
+  return <div className={styles.mainWrapper}></div>;
+};
+
+export default MainPage;
